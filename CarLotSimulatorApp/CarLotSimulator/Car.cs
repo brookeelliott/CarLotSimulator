3c3dac4f371b991e0abe10e8adfefe849245f9f0
Brooke Elliott <brookeelliott711@gmail.com>
2023-06-06 21:08:54 -0600

    exercise2

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace CarLotSimulator
+{
+	public class Car
+	{
+		
+
+			public string Make { get; set; }
+			public string Model { get; set; }
+			public int year { get; set; }
+			public string EngineNoise { get; set; }
+			public string HonkNoise { get; set; }
+			public bool IsDriveable { get; set; }
+
+			public void MakeEngineNoise(string engineInput)
+			{
+			Console.WriteLine($"{engineInput}!!!!!!!");
+			}
+
+			public void MakeHonkNoise(string honkInput)
+			{
+			Console.WriteLine($"{honkInput}!!!!!!!");
+            }
+
+
+	
+	}
+}
+
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -18,6 +18,42 @@ namespace CarLotSimulator
             //Set the properties for each of the cars
             //Call each of the methods for each car
 
+            Car car1 = new Car();
+            Car car2 = new Car();
+            Car car3 = new Car();
+
+            car1.Make = "Dodge";
+            car1.Model = "Durango";
+            car1.year = 2000;
+            car1.EngineNoise = "RRRRAAAAAAAAAUUUUUUHHHHHHHHH (BOOM CLATTER CLACK TTSSSS)";
+            car1.HonkNoise = "BEEP BEEP";
+            car1.IsDriveable = false;
+
+            car1.MakeEngineNoise(car1.EngineNoise);
+            car1.MakeHonkNoise(car1.HonkNoise);
+
+            car2.Make = "Honda";
+            car2.Model = "Civic";
+            car2.year = 2012;
+            car2.EngineNoise = "BZZZZZZZZZZZ";
+            car2.HonkNoise = "BOOP";
+            car2.IsDriveable = true;
+
+            car2.MakeEngineNoise(car2.EngineNoise);
+            car2.MakeHonkNoise(car2.HonkNoise);
+
+            car3.Make = "Ram";
+            car3.Model = "2500";
+            car3.year = 1998;
+            car3.EngineNoise = "RUMBLERUMBLERUMBLERUMBLERUMBLERUMBLE";
+            car3.HonkNoise = "Blep";
+            car3.IsDriveable = true;
+
+            car3.MakeEngineNoise(car3.EngineNoise);
+            car3.MakeHonkNoise(car3.HonkNoise);
+
+
+            //Ehh, another day on these
             //*************BONUS*************//
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
